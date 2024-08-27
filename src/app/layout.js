5269d785fac04c170fc4b9298020c14bced69b68
Karrot <karrot@Karrotui-iMac.local>
2024-08-27 17:29:14 +0900

    SEO setting

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 
 
@@ -7,14 +8,35 @@ import "./globals.css";
 export const metadata = {
   title: "Youngand",
   description: "Transform Your Health with Digital Therapeutics",
+  keywords: "digital therapeutics, health, wellness, cognitive training, dementia prevention",
+  author: "youngand",
+  url: "https://youngandx.com",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+      <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta name="author" content={metadata.author} />
+
+        {/* Open Graph (Facebook, LinkedIn) */}
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:url" content={metadata.url} />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+
+      </Head>
       <body>
-          {children}  
-        </body>
-    </html>
+      {children}
+      </body>
+      </html>
   );
 }
