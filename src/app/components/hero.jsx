1008376af2605ab95db06800b626d86c32e11361
Karrot <karrot@Karrotui-iMac.local>
2024-09-10 10:49:14 +0900

    coming soon

--- src/app/components/hero.jsx
+++ src/app/components/hero.jsx
@@ -17,21 +17,28 @@ export default function Hero({ scrollToContact }) {
                         height={400} // 세로 크기 설정
                     />
                 </div>
-                <div className="flex flex-col items-center text-center md:w-1/2 md:items-start md:pl-16 md:text-left lg:flex-grow lg:pl-24">
-                    <h1 className="title-font mb-4 text-3xl font-medium text-gray-100 sm:text-4xl">
-                        Brain Health Playground
-                    </h1>
-                    <p className="mb-8 leading-relaxed">
-                        This digital healthcare solution uses AI and computer vision to detect early signs of dementia and deliver personalized cognitive training programs. Our system provides an effective approach for dementia detection and prevention, helping patients maintain and enhance their cognitive function.
-                    </p>
-                    <div className="flex justify-center w-full md:justify-start">
-                        <button 
-                            onClick={scrollToContact} // 여기서 onClick 핸들러를 제대로 설정합니다.
-                            className="inline-flex items-center rounded-lg bg-gray-100 px-5 py-3 hover:bg-cyan-900 hover:text-white focus:outline-none"
-                        >
-                            <span className="title-font font-medium">Contact Us</span>
-                        </button>
-                    </div>
+                <div
+                    className="flex flex-col items-center text-center md:w-1/2 md:items-start md:pl-16 md:text-left lg:flex-grow lg:pl-24">
+                        <p className="mb-0 leading-relaxed text-amber-300">
+                            Coming this February 2025 – Prepare for the next big thing!
+                        </p>
+                        <h1 className="title-font mb-4 text-3xl font-medium text-gray-100 sm:text-4xl">
+                            Brain Health Playground
+                        </h1>
+                        <p className="mb-8 leading-relaxed">
+                            This digital healthcare solution uses AI and computer vision to detect early signs of
+                            dementia and deliver personalized cognitive training programs. Our system provides an
+                            effective approach for dementia detection and prevention, helping patients maintain and
+                            enhance their cognitive function.
+                        </p>
+                        <div className="flex justify-center w-full md:justify-start">
+                            <button
+                                onClick={scrollToContact} // 여기서 onClick 핸들러를 제대로 설정합니다.
+                                className="inline-flex items-center rounded-lg bg-gray-100 px-5 py-3 hover:bg-cyan-900 hover:text-white focus:outline-none"
+                            >
+                                <span className="title-font font-medium">Contact Us</span>
+                            </button>
+                        </div>
                 </div>
             </div>
         </section>
