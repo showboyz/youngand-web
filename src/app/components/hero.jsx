948bea1e36fed4c61d450e5c8aaba3458c608c1a
Your Name <your.email@example.com>
2025-08-12 15:13:15 +0900

    chore: hero 섹션 문구 업데이트

--- src/app/components/hero.jsx
+++ src/app/components/hero.jsx
@@ -19,9 +19,9 @@ export default function Hero({ scrollToContact }) {
                 </div>
                 <div
                     className="flex flex-col items-center text-center md:w-1/2 md:items-start md:pl-16 md:text-left lg:flex-grow lg:pl-24">
-                        <p className="mb-0 leading-relaxed text-amber-300">
+                        {/* <p className="mb-0 leading-relaxed text-amber-300">
                             Coming this February 2025 – Prepare for the next big thing!
-                        </p>
+                        </p> */}
                         <h1 className="title-font mb-4 text-3xl font-medium text-gray-100 sm:text-4xl">
                             Brain Health Playground
                         </h1>
